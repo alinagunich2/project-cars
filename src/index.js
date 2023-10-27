2631d62fd63af9344e7f0cc0c0c2226d65ed1f94
unknown <gunich.alina@gmail.com>
2023-10-27 17:34:28 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import './index.html';
 import './index.scss';
-
-import { mult, sum } from './modules/index';
-
-const imgWrap = document.querySelector('.img');
-const img = new Image();
-img.src = code;
-img.width = 700;
-imgWrap.append(img);
-
-console.log(mult(3, 4));
-console.log(sum(3, 4));
+import './modules/index';
